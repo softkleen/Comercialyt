ec00ad2f03459d426b654ac6627fe1dc0f9dd766
Wellington Santos <wellington.vsantos@gmail.com>
2021-06-15 11:50:58 -0300

    implementando classe cliente

--- Comercialon/Classes/Cliente.cs
+++ Comercialon/Classes/Cliente.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Comercialon.Classes
 {
-    class Cliente
+    public class Cliente
     {
+        //declaração de atributo
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        // métodos construtores
+        public Cliente()
+        {
+        }
+        public Cliente(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+        // métodos da classe
+        public void Inserir() 
+        { }
+        public bool Alterar(int id) 
+        {
+            return true;
+        }
+        public static List<Cliente> ListarTodos() 
+        {
+            List<Cliente> lista = new List<Cliente>();
+            // code de listar ...
+            return lista;
+        }
+        public void BuscarPorId(int id) 
+        { 
+            
+        }
     }
 }
